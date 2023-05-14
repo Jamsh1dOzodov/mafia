b1adb98db2e4f9ac7669b89e0f3a7d9d02448a8f
Jamsh1dOzodov <o.jamshidbek12@gmail.com>
2023-05-14 13:20:53 +0500

    divided into tabs

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ purchase_btns.forEach(btn => {
     btn.addEventListener('click', event => {
         const el = event.target
         const parent = el.closest('.purchase-amount')
-        const input = parent.querySelector('.purchase-amount__number')
-        if(el.classList.contains('purchase-amount__minus')){
+        const input = parent.querySelector('.purchase-amount__count')
+        if(el.classList.contains('purchase-amount__btn--minus')){
             // Дикримент
             input.value--
             if(input.value<=0){
@@ -19,3 +19,5 @@ purchase_btns.forEach(btn => {
         console.log(input.value)
     })
 })
+
+
