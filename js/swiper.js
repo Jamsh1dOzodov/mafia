3430f159e2b0203d1f8730b52f7244f357453b2d
Jamsh1dOzodov <o.jamshidbek12@gmail.com>
2023-06-26 14:12:58 +0500

    added simproduct cards

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.home-simproducts__swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.simproducts-btn--right',
+      prevEl: '.simproducts-btn--left',
+    },
+  
+  });
