655ab7a61a6f39c32ca501bfdbc8dfa9bdc91a95
Jamsh1dOzodov <o.jamshidbek12@gmail.com>
2023-08-06 21:35:34 +0500

    added validation form for checkout page

--- js/index.js
+++ js/index.js
@@ -5,14 +5,14 @@ purchase_btns.forEach(btn => {
         const el = event.target
         const parent = el.closest('.purchase-amount')
         const input = parent.querySelector('.purchase-amount__count')
-        if(el.classList.contains('purchase-amount__btn--minus')){
+        if (el.classList.contains('purchase-amount__btn--minus')) {
             // Дикримент
             input.value--
-            if(input.value<=0){
+            if (input.value <= 0) {
                 input.value = 0
             }
         }
-        else{
+        else {
             // Инкримент
             input.value++
         }
@@ -24,12 +24,12 @@ const openTabs = (event, tabName) => {
     let i, tabContent, tabLinks;
 
     tabContent = document.querySelectorAll(".tabContent")
-    for(i = 0; i < tabContent.length; i++){
+    for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none"
     }
 
     tabLinks = document.querySelectorAll(".description-list__item")
-    for(i = 0; i < tabLinks.length; i++){
+    for (i = 0; i < tabLinks.length; i++) {
         tabLinks[i].className = tabLinks[i].className.replace(" description-list__item--active", "")
     }
 
@@ -39,16 +39,40 @@ const openTabs = (event, tabName) => {
 
 
 const formEl = document.querySelector('.review');
-
 formEl.addEventListener('submit', (event) => {
     event.preventDefault()
     const emailInp = formEl.email
     const emailReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    
-    if(emailInp.value){
+
+    if (emailInp.value.match(emailReg)) {
         emailInp.style.border = '1px solid green'
     }
-    else{
+    else {
         emailInp.style.border = '1px dashed red'
+    }
+})
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault()
+    const nameInp = formEl.name
+    const nameReg = /^[a-zA-Z]+$/
+    if (nameInp.value.match(nameReg)) {
+        nameInp.style.border = '1px solid green'
+    }
+    else {
+        nameInp.style.border = '1px dashed red'
+    }
+})
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault()
+    const commentInp = formEl.comment
+    const commentReg = /^[A-Za-zА-Яа-яЁё\s]{6,}/
+
+    if(commentInp.value.match(commentReg)){
+        commentInp.style.border = '1px solid green'
+    }
+    else{
+        commentInp.style.border = '1px dashed red'
     }
 })
