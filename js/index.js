c4c0e3c6d59f26b4d769329afa924552166815ac
Jamsh1dOzodov <o.jamshidbek12@gmail.com>
2023-09-20 21:03:51 +0500

    added new page catalog

--- js/index.js
+++ js/index.js
@@ -58,114 +58,175 @@ const openTabs = (event, tabName) => {
 
 
 const reviewBtn = document.querySelector('.review__btn');
-reviewBtn.addEventListener('click', event => {
-    const reviewInputs = document.querySelectorAll('.review__input')
-    let data = {}
-    reviewInputs.forEach(input => {
-        let regex;
-        if (input.classList.contains('review__input--name')) {
-            regex = /^[a-zA-Z]+$/
-            if (input.value.match(regex)) {
-                input.classList.remove('review__input--error')
-                data.name = input.value
+if (reviewBtn) {
+    reviewBtn.addEventListener('click', event => {
+        const reviewInputs = document.querySelectorAll('.review__input')
+        let data = {}
+        reviewInputs.forEach(input => {
+            let regex;
+            if (input.classList.contains('review__input--name')) {
+                regex = /^[a-zA-Z]+$/
+                if (input.value.match(regex)) {
+                    input.classList.remove('review__input--error')
+                    data.name = input.value
+                }
+                else {
+                    input.classList.add('review__input--error')
+                }
             }
-            else {
-                input.classList.add('review__input--error')
+            if (input.classList.contains('review__input--email')) {
+                regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                if (input.value.match(regex)) {
+                    input.classList.remove('review__input--error')
+                    data.email = input.value
+                }
+                else {
+                    input.classList.add('review__input--error')
+                }
             }
-        }
-        if (input.classList.contains('review__input--email')) {
-            regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-            if (input.value.match(regex)) {
-                input.classList.remove('review__input--error')
-                data.email = input.value
+            if (input.classList.contains('review__input--comment')) {
+                regex = /^[A-Za-zА-Яа-яЁё\s]{6,}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('review__input--error')
+                    data.comment = input.value
+                }
+                else {
+                    input.classList.add('review__input--error')
+                }
             }
-            else {
-                input.classList.add('review__input--error')
-            }
-        }
-        if (input.classList.contains('review__input--comment')) {
-            regex = /^[A-Za-zА-Яа-яЁё\s]{6,}/
-            if (input.value.match(regex)) {
-                input.classList.remove('review__input--error')
-                data.comment = input.value
-            }
-            else {
-                input.classList.add('review__input--error')
-            }
-        }
 
-        if(Object.keys(data).length == 3) {
-            const reviewModal = document.querySelector('.modal')
-            reviewModal.classList.add('modal--active')
-        }
+            if (Object.keys(data).length == 3) {
+                const reviewModal = document.querySelector('.modal')
+                reviewModal.classList.add('modal--active')
+            }
+        })
     })
-})
+}
+
 
 
 
 const reviewModal = document.querySelector('.modal')
-reviewModal.addEventListener('click', event => {
-    reviewModal.classList.remove('modal--active')
-})
+if (reviewModal) {
+    reviewModal.addEventListener('click', event => {
+        reviewModal.classList.remove('modal--active')
+        location.reload()
+    })
+}
+
 
 
 
 
 const checkoutBtn = document.querySelector('.cart-order__btn--checkout');
-checkoutBtn.addEventListener('click', event => {
-    const formInp = document.querySelectorAll('.formInp')
-    let  data = {}
-    formInp.forEach(input => {
-        let regex;
-        if (input.classList.contains('formInp--name')) {
-            regex = /^[a-zA-Z]+$/
-            if (input.value.match(regex)) {
-                input.classList.remove('formInp--error')
-                data.name = input.value
+if (checkoutBtn) {
+    checkoutBtn.addEventListener('click', event => {
+        const formInp = document.querySelectorAll('.formInp')
+        let data = {}
+        formInp.forEach(input => {
+            let regex;
+            if (input.classList.contains('formInp--name')) {
+                regex = /^[a-zA-Z]+$/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.name = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-            else {
-                input.classList.add('formInp--error')
+
+            if (input.classList.contains('formInp--phone')) {
+                regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.phone = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-        }
-        if (input.classList.contains('formInp--phone')) {
-            regex = /^\+9-\d{3}-\d{3}-\d{2}-\d{2}$/
-            if (input.value.match(regex)) {
-                input.classList.remove('formInp--error')
-                data.name = input.value
+
+            if (input.classList.contains('formInp--email')) {
+                regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.email = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-            else {
-                input.classList.add('formInp--error')
+
+            if (input.classList.contains('formInp--coupon')) {
+                regex = /\d{5}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.coupon = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-        }
-        if (input.classList.contains('formInp--email')) {
-            regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-            if (input.value.match(regex)) {
-                input.classList.remove('formInp--error')
-                data.name = input.value
+
+            if (input.classList.contains('formInp--homeNumber')) {
+                regex = /\d{2}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.homeNumber = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-            else {
-                input.classList.add('formInp--error')
+
+            if (input.classList.contains('formInp--kvartNumber')) {
+                regex = /\d{2}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.kvartNumber = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-        }
-        if (input.classList.contains('formInp--number')) {
-            regex = /\d{5}/
-            if (input.value.match(regex)) {
-                input.classList.remove('formInp--error')
-                data.name = input.value
+
+            if (input.classList.contains('formInp--city')) {
+                regex = /^[A-Za-zА-Яа-яЁё\s]{2,}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.city = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-            else {
-                input.classList.add('formInp--error')
+
+            if (input.classList.contains('formInp--street')) {
+                regex = /^[A-Za-zА-Яа-яЁё\s]{2,}/
+                if (input.value.match(regex)) {
+                    input.classList.remove('formInp--error')
+                    data.street = input.value
+                }
+                else {
+                    input.classList.add('formInp--error')
+                }
             }
-        }
-        if (input.classList.contains('formInp--text')) {
-            regex = /^[A-Za-zА-Яа-яЁё\s]{2,}/
-            if (input.value.match(regex)) {
-                input.classList.remove('formInp--error')
-                data.name = input.value
+
+            if (Object.keys(data).length == 8) {
+                const reviewModal = document.querySelector('.modal')
+                reviewModal.classList.add('modal--active')
             }
-            else {
-                input.classList.add('formInp--error')
-            }
-        }
+        })
+    })
+}
+
+
+
+const paymentBtns = document.querySelectorAll('.payment-form__btn')
+paymentBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.payment-form__btn--active').classList.remove('payment-form__btn--active');
+        btn.classList.add('payment-form__btn--active');
     })
 })
